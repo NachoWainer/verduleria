b7b17575ed8d7f7ea5b277b3e7cf69b72b16f3b7
Ignacio Wainer <nachowainer@gmail.com>
2022-12-11 19:42:09 -0300

    productos

---
+++ products.js
@@ -0,0 +1,75 @@
+const frutas = [
+{
+    name: "MANZANA",
+    price: 200,
+    img:"",
+},
+{
+    name: "BANANA",
+    price: 200,
+    img:"",
+},
+{
+    name: "PERA",
+    price: 200,
+    img:"",
+
+},
+{
+    name: "KIWI",
+    price: 200,
+    img:"",
+
+},
+{
+    name: "FRUTILLA",
+    price: 200,
+    img:"",
+
+},
+{
+    name: "NARANJA",
+    price: 200,
+    img:"",
+
+},
+];
+
+const verduras = [
+    {
+    name: "LECHUGA",
+    price: 200,
+    img:"",
+    },
+    {
+        name: "TOMATE",
+        price: 200,
+        img:"",
+    },
+    {
+        name: "CEBOLLA",
+        price: 200,
+        img:"",
+    
+    },
+    {
+        name: "ZANAHORIA",
+        price: 200,
+        img:"",
+    
+    },
+    {
+        name: "PALTA",
+        price: 200,
+        img:"",
+    
+    },
+    {
+        name: "ZAPALLO",
+        price: 200,
+        img:"",
+    
+    },
+];
+
+const carrito = [];
